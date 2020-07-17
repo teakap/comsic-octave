e179254f1626069c2028e0c6cc4d9ca4cda962fd
Tasos Kapetzoglou <tasos.kapetzoglou@gmail.com>
2020-07-17 19:32:45 +0200

    added react-bootstrap and react-router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-// import React from 'react';
-// import logo from './logo.svg';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 import React from "react";
 import "./App.css";
 import { AudioPlayerProvider } from "react-use-audio-player";
-import AudioPlayer from './AudioPlayer'
+import AudioPlayer from "./AudioPlayer";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const App = () => {
   return (
-    <AudioPlayerProvider>
-      <AudioPlayer file="asdf.mp3" />
-    </AudioPlayerProvider>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Cosmic Octave</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Sun</Nav.Link>
+            <Nav.Link href="#link">Moon</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          {/* <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+        </Navbar.Collapse>
+      </Navbar>
+      <AudioPlayerProvider>
+        <AudioPlayer file="asdf.mp3" />
+      </AudioPlayerProvider>
+    </div>
   );
 };
 export default App;
